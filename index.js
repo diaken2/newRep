962afc38a5cd18554bc45232198be62325f29eff
diaken2 <kenansalahov111@gmail.com>
2023-08-10 21:03:27 +0500

    anime

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-console.log('dddddd2')
-console.log('ooookkkkkk')
-const a=5
-let A=[]
-for (let i=0;i<5;i++){
-A.push(i)
-}
-console.log(A)
-let B = A.reduce((add,cor)=>{
-if (cor==2){
-add.push(cor)
-}
-return add
-},[])
-console.log(B)
 console.log('АНИМЕ')
